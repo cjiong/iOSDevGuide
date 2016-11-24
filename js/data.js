871a39fcd1fc342a338a7b83d661e2a8043ca431
cjiong <416137318@qq.com>
2016-11-24 23:52:24 +0800

    update data.js

--- js/data.js
+++ js/data.js
@@ -6,7 +6,8 @@ var iOSDevGuide = {
 			"pageC2":[
 			{'Name':'慕课网','Title':'在线视频教程，在线代码练习','Href':'http://www.imooc.com/'},
 			{'Name':'实验楼','Title':'第一家以实验为核心的IT在线教育平台','Href':'https://www.shiyanlou.com/'},
-			{'Name':'极客学院','Title':'在线视频教学，强大的知识体系','Href':'http://www.jikexueyuan.com/'}
+			{'Name':'极客学院','Title':'在线视频教学，强大的知识体系','Href':'http://www.jikexueyuan.com/'},
+			{'Name':'萌码','Title':'在线编程学习，实时问答，交互学习','Href':'http://www.mengma.com/'}
 			]
 		}, {
 			
