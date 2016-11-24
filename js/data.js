a455c54f07030891c541516c9c7a0b4c175ab10f
cjiong <416137318@qq.com>
2016-11-24 23:45:31 +0800

    update data.js

--- js/data.js
+++ js/data.js
@@ -4,7 +4,7 @@ var iOSDevGuide = {
 		"pageC1":[{
 			"pageT2":iOS教程,
 			"pageC2":[
-			{'Name':'慕课网','Title':'在线视频教程，在线代码练习','Href':'http://www.imooc.com/',"New":"1"},
+			{'Name':'慕课网','Title':'在线视频教程，在线代码练习','Href':'http://www.imooc.com/'},
 			{'Name':'实验楼','Title':'第一家以实验为核心的IT在线教育平台','Href':'https://www.shiyanlou.com/'},
 			{}
 			]
