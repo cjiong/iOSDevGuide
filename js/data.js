908fbb54f9ab7fd4979702928594565b115acd9a
cjiong <416137318@qq.com>
2016-11-27 23:43:07 +0800

    update

--- js/data.js
+++ js/data.js
@@ -8,6 +8,7 @@ var iOSDevGuide = {
 			{'Name':'实验楼','Title':'第一家以实验为核心的IT在线教育平台','Href':'https://www.shiyanlou.com/'},
 			{'Name':'极客学院','Title':'在线视频教学，强大的知识体系','Href':'http://www.jikexueyuan.com/'},
 			{'Name':'萌码','Title':'在线编程学习，实时问答，交互学习','Href':'http://www.mengma.com/'}
+			{'Name':'百度传课','Title':'在线视频学习','Href':'http://www.chuanke.com/course/_ios_____.html'}
 			]
 		}, {
 			
