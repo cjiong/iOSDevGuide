0fa4f3de60787159d6ce5dd1cb020b8d1df9ac6c
cjiong <416137318@qq.com>
2016-12-16 23:38:51 +0800

    update data.js

--- js/data.js
+++ js/data.js
@@ -14,7 +14,7 @@ var iOSDevGuide = {
 			"pageT2":"iOS知识",
 			"pageC2":[
 			{'Name':'SwiftGG','Title':'走心的 Swift 翻译组','Href':'http://swift.gg/'},
-			{}
+			{'Name':'','Title':'','Href':''}
 			]
 		}, {
 			"pageT2":"开发社区",
@@ -27,7 +27,8 @@ var iOSDevGuide = {
 			{'Name':'知乎','Title':'与世界分享你的知识、经验和见解','Href':'http://www.zhihu.com/topics'},
 			{'Name':'简书','Title':'交流故事，沟通想法','Href':'http://www.jianshu.com/'},
 			{'Name':'Segmentfault','Title':'一个专注于解决编程问题，提高开发技能的社区','Href':'https://segmentfault.com/'},
-			{'Name':'cocoachina','Title':'让移动开发更简单','Href':'http://www.cocoachina.com/'}
+			{'Name':'cocoachina','Title':'让移动开发更简单','Href':'http://www.cocoachina.com/'},
+			{'Name':'博客园','Title':'开发者的网上家园','Href':'http://www.cnblogs.com/'}
 			]
 		}]}, {//大块
 		"pageT1":"Swift/Objective-C",	
