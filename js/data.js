de820220b6e16a947dd9f646eff3b9068db696a6
cjiong <416137318@qq.com>
2016-12-16 23:01:33 +0800

    update data.js

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@ var iOSDevGuide = {
 	"main":[{
 		"pageT1":"圈子",
 		"pageC1":[{
-			"pageT2":开发教程,
+			"pageT2":"开发教程",
 			"pageC2":[
 			{'Name':'慕课网','Title':'在线视频教程，在线代码练习','Href':'http://www.imooc.com/'},
 			{'Name':'实验楼','Title':'第一家以实验为核心的IT在线教育平台','Href':'https://www.shiyanlou.com/'},
@@ -11,7 +11,7 @@ var iOSDevGuide = {
 			{'Name':'百度传课','Title':'在线视频学习','Href':'http://www.chuanke.com/course/_ios_____.html'}
 			]
 		}, {
-			"pageT2":开发社区,
+			"pageT2":"开发社区",
 			"pageC2":[
 			{'Name':'Github','Title':'Share Code','Href':'https://github.com/'},
 			{'Name':'Stackoverflow','Title':'','Href':'http://stackoverflow.com/'},
@@ -22,6 +22,22 @@ var iOSDevGuide = {
 			{'Name':'简书','Title':'','Href':'http://www.jianshu.com/'},
 			{'Name':'segmentfault','Title':'专注于程序员开发者的社区平台，提供开发相关的高质量问答，专栏，笔记，招聘，线下活动等服务。','Href':'http://segmentfault.com/'},
 			{'Name':'cocoachina','Title':'','Href':'http://www.cocoachina.com/'}
+			]
+		}]}, {
+		"pageT1":"Swift/Objective-C",	
+		"pageC1":[{
+			"pageT2":"Swift",
+			"pageC2":[
+			{},
+			{},
+			{}
+			]
+		}, {
+			"pageT2":"Objective-C",
+			"pageC2":[
+			{},
+			{},
+			{}
 			]
 		}]
 	}]
